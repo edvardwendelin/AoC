5bc729dce9d0f245d4baec73d62f3e5cebe5894f
Edvard Wendelin <edvard@spotify.com>
2020-12-12 09:41:57 +0100

    Day 10 cleanup

--- day10/day10_2.rb
+++ day10/day10_2.rb
@@ -1,13 +1,13 @@
 numbers     = STDIN.readlines().map(&:to_i)
 numbers.sort!
-numbers.push(numbers.last+3)    # add device
-numbers.unshift(0)              # add power otutlet
+numbers.push(numbers.last+3)    # add device to end of array
+numbers.unshift(0)              # add power otutlet to beginning of array
 results = 1
 
 i = 1
 ones = 0
-while i < numbers.length()  # iterate over all elements in list
-    case numbers[i]-numbers[i-1] # look at specific element in list at position c
+while i < numbers.length()  
+    case numbers[i]-numbers[i-1] 
     when 1            
         ones+=1
     when 3
@@ -20,9 +20,9 @@ while i < numbers.length()  # iterate over all elements in list
             results *=2
         end 
 
-        ones = 0       # reset ones counter
+        ones = 0      
     end
-    i+=1        #  Jump to next row in list of distances
+    i+=1
 end
 
 puts results
